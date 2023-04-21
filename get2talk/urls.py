4ec13d2635382d8f272724486572522f1f192711
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-04-21 12:22:00 +0200

    Create a simple dashboard and Add Student and Edit options

--- get2talk/urls.py
+++ get2talk/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import AddNewStudentView, EditStudentView
 
 urlpatterns = [
-    path("", views.login_view, name="login_view"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("logout/", views.logout_view, name="logout_view"),
-    path("add/", views.add_new_student, name="add_new_student"),
-    path("hours/", views.working_hours, name="working_hours"),
+    path("", views.login_view, name="login"),
+    path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("logout/", views.logout_view, name="logout"),
+    path("add/", AddNewStudentView.as_view(), name="add_new_student"),
+    path("edit/<int:pk>/", EditStudentView.as_view(), name="edit_student"),
+    path("hours/", views.working_hours_view, name="working_hours"),
 ]
--- get2talk/views.py
+++ get2talk/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from .forms import StudentForm
+from .models import Teacher, Student
 
 
 def login_view(request):
@@ -8,10 +14,8 @@ def login_view(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
-        print(user)
         if user is not None:
             login(request, user)
-            print("Success")
             return redirect("dashboard")
         else:
             error_message = "Invalid login credentials"
@@ -21,19 +25,47 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect("login_view")
+    return redirect("login")
 
 
 @login_required()
-def dashboard(request):
-    return render(request, "dashboard.html")
+def dashboard_view(request):
+    teacher = Teacher.objects.get(user=request.user)
+    students = Student.objects.filter(teacher=teacher)
+    context = {"teacher": teacher, "students": students}
+    return render(request, "dashboard.html", context)
+
+
+@method_decorator(login_required, name="dispatch")
+class EditStudentView(View):
+    template_name = "edit_student.html"
+
+    def get(self, request, pk):
+        student = Student.objects.get(id=pk)
+        form = StudentForm(instance=student)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, pk):
+        student = Student.objects.get(id=pk)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+        return render(request, self.template_name, {"form": form})
+
+
+@method_decorator(login_required, name="dispatch")
+class AddNewStudentView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "new_student.html"
+    success_url = reverse_lazy("dashboard")
+
+    def form_valid(self, form):
+        form.instance.teacher = self.request.user.teacher
+        return super().form_valid(form)
 
 
 @login_required
-def add_new_student(request):
-    return render(request, "new_student.html")
-
-
-@login_required
-def working_hours(request):
+def working_hours_view(request):
     return render(request, "working_hours.html")
