19bf7df57cff024b8b5a23d52caab99aa3d7476b
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-06-06 19:37:06 +0200

    Adjust admin view with suggestion model

--- get2talk/admin.py
+++ get2talk/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Teacher, Student, Lesson
+from .models import Teacher, Student, Lesson, Suggestion
 
 
-# Register your models here.
 class LessonAdmin(admin.ModelAdmin):
     list_display = ("student", "teacher", "date", "duration")
     list_filter = ("teacher", "student", "date")
@@ -13,6 +12,12 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ("teacher",)
 
 
+class SuggestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "user")
+    list_filter = ("date_submitted", "user")
+
+
 admin.site.register(Teacher)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Suggestion, SuggestionAdmin)
